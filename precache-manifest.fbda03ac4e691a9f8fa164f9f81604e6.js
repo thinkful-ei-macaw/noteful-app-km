e84936dda8a1c5694aa0b092966b3602517c6f42
Malcolm Kiano <malcolmkiano@gmail.com>
2020-03-10 16:40:29 -0400

    Updates

---
+++ precache-manifest.fbda03ac4e691a9f8fa164f9f81604e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3eeba4b0eb9688dc0bb861e14133f13",
+    "url": "/noteful-app-km/index.html"
+  },
+  {
+    "revision": "9b44eae5f3191689d66e",
+    "url": "/noteful-app-km/static/css/main.921e7250.chunk.css"
+  },
+  {
+    "revision": "a46db5900655311a131a",
+    "url": "/noteful-app-km/static/js/2.46b13d0e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/noteful-app-km/static/js/2.46b13d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b44eae5f3191689d66e",
+    "url": "/noteful-app-km/static/js/main.98df4efe.chunk.js"
+  },
+  {
+    "revision": "722a489e33c107016581",
+    "url": "/noteful-app-km/static/js/runtime-main.2c5a1637.js"
+  }
+]);
