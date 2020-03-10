d36f2b828846580af1ddc66ef61374073ff11cd1
Malcolm Kiano <malcolmkiano@gmail.com>
2020-03-10 15:38:12 -0400

    Added goBack functionality

--- src/Components/SelectedFolder.js
+++ src/Components/SelectedFolder.js
@@ -3,14 +3,12 @@ import React from 'react';
 class SelectedFolder extends React.Component {
 
     render() {
-      console.log(this.props)
-
       let note = this.props.data.notes.find(n => n.id === this.props.match.params.id);
       let folder = this.props.data.folders.find(f => f.id === note.folderId);
 
       return (
         <article>
-          <button>Go Back</button>
+          <button onClick={() => this.props.history.goBack()}>Go Back</button>
           <h3>{folder.name}</h3>
         </article>
       )
