3c08f12da3de8673364993dcda5ddcac730df485
Keith Freitag <keithfreitag@gmail.com>
2020-03-10 08:43:58 -0700

    greyframe

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import store from './store'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = store
+
+  render() {
+    return (
+      <main>
+        <section className='sidebar'>
+            <ul>
+                <li>Folder 1</li>
+                <li>Folder 2</li>
+                <li>Folder 3</li>
+                <li><button>Add Folder</button></li>
+            </ul>
+            <article><button>Go Back</button>
+                <h3>Folder X</h3>
+            </article>
+        </section>
+        <section className='content'>
+            <ul>
+                <li>
+                    <h2>Note 1</h2>
+                    <p>
+                        Date modified ...
+                    </p>
+                    <button>Delete Note</button>
+                </li>
+                <li>
+                    <h2>Note 1</h2>
+                    <p>
+                        Date modified ...
+                    </p>
+                    <button>Delete Note</button>
+                </li>
+                <li>
+                    <h2>Note 1</h2>
+                    <p>
+                        Date modified ...
+                    </p>
+                    <button>Delete Note</button>
+                </li>
+                <li>
+                    <button>Add Note</button>
+                </li>
+            </ul>
+            <article><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nihil sapiente 
+                officiis eius consectetur tenetur, libero cum numquam delectus at quasi, minus exercitationem quos, quo ad temporibus 
+                consequuntur optio vero alias adipisci molestiae culpa deleniti. Deleniti deserunt possimus, dicta tenetur eligendi 
+                dolor officiis illum quo error unde adipisci molestiae atque, laborum odit neque minus eius. Eveniet esse voluptates 
+                nostrum nobis hic placeat optio sint itaque ducimus magni cum delectus sequi magnam id praesentium, illo qui assumenda 
+                natus suscipit quaerat aliquam.</p></article>
+        </section>
+
+    </main>
+    )
+  }
+
 }
+
+
+
+
 
 export default App;
